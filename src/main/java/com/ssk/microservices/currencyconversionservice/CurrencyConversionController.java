0a8cd457fa9290cc45fe6b37c15ab24db047fee6
Sagar Singh Khati <sonu06sagar@gmail.com>
2023-04-11 22:30:38 +0530

    Added openfeign to invoke currency-exchange-service

--- src/main/java/com/ssk/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/ssk/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,19 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+
+	@Autowired
+	CurrencyExchangeServiceProxy proxy;
+
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+				quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
@@ -29,4 +43,5 @@ public class CurrencyConversionController {
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
 //		return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
 	}
+
 }
--- src/main/java/com/ssk/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/ssk/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,7 +2,9 @@ package com.ssk.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.ssk.microservices.currencyconversionservice")
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
